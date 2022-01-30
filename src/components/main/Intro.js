c232a810ca5d1a4d11790889bdd55b620d594a10
uvin1016 <uvin1016@naver.com>
2022-01-31 00:10:59 +0900

    스크롤 할때마다 title 무빙 추가

--- src/components/main/Intro.js
+++ src/components/main/Intro.js
@@ -1,13 +1,32 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function Intro(){
     const path = process.env.PUBLIC_URL;
     const [position, setPosition] = useState({x:0,y:0});
+    const tit = useRef(null);
     let scrollY = document.documentElement.scrollTop;
+
+    const handleScroll = ()=>{
+        const title = tit.current.querySelectorAll("h1");
+        let scroll = window.scrollY || window.pageYOffset;
+        
+        if(scroll > 0) {
+            title[0].style.transform = `translateX(${scroll}px)`;
+            title[1].style.transform = `translateX(-${scroll}px)`;
+        }
+    }
+    
+    useEffect(()=>{
+        window.addEventListener('scroll',handleScroll);
+
+        return ()=>{
+            window.removeEventListener('scroll',handleScroll);
+        }
+    },[])
 
     return(
         <section id="intro">
-            <div className="titleBox">
+            <div className="titleBox" ref={tit}>
                 <h1>WELLCOME TO UFOLIO I'M</h1>
                 <h1>FRONTEND DEVELOPER</h1>
             </div>
@@ -17,7 +36,7 @@ function Intro(){
                     setPosition({x: e.clientX, y: e.clientY+scrollY});
                 }}>신입 프론트엔드 개발자 이유빈입니다.
                     <div className="imgBox" style={{left: `${position.x}px`, top: `${position.y}px`}}>
-                        <img src={`${path}/img/pf_img.jpg`}  />
+                        <img src={`${path}/img/myimg.jpg`}  />
                     </div> 
                 </div>
                 제가 상상하던 디자인이 문자를 통해 시각적으로 실현되는 과정을 좋아합니다. 앞으로 사용자의 편리함과 시각적인 즐거움을 실용적으로 구현해낼 수 있도록 배움을 놓지 않겠습니다.
